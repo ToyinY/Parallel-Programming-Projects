497a8087ddbf4a4dfb23aed1992c67003119b208
toyiny <yusuf.f@husky.neu.edu>
2019-04-08 12:56:11 -0400

    uchar4 not found

--- cuda/imageBlur/image_blur.cu
+++ cuda/imageBlur/image_blur.cu
@@ -3,13 +3,11 @@
 #include <cuda.h>
 #include <time.h>
 
-unsigned char *input, 
-unsigned char *output,
-float filter;
+unsigned char *red;
+unsigned char *green;
+unsigned char *blue;
+float *d_filter;
 
-/*******************************************************/
-/*                 Cuda Error Function                 */
-/*******************************************************/
 inline cudaError_t checkCuda(cudaError_t result) {
 	#if defined(DEBUG) || defined(_DEBUG)
 		if (result != cudaSuccess) {
@@ -20,18 +18,84 @@ inline cudaError_t checkCuda(cudaError_t result) {
 		return result;
 }
 
+__global__ void separateChannels(const uchar4* const inputImageRGBA,
+                      int numRows,
+                      int numCols,
+                      unsigned char* const redChannel,
+                      unsigned char* const greenChannel,
+                      unsigned char* const blueChannel)
+{
+  int px = blockIdx.x * blockDim.x + threadIdx.x;
+  int py = blockIdx.y * blockDim.y + threadIdx.y;
+  if (px >= numCols || py >= numRows) {
+      return;
+  }
+  int i = py * numCols + px;
+  redChannel[i] = inputImageRGBA[i].x;
+  greenChannel[i] = inputImageRGBA[i].y;
+  blueChannel[i] = inputImageRGBA[i].z;
+}
 
-int imageBlur (unsigned char *input,
-			   unsigned char *output,
-			   unsigned int rows,
-			   unsigned int cols) {
+__global__ void combineChannels(const unsigned char* const redChannel,
+                       const unsigned char* const greenChannel,
+                       const unsigned char* const blueChannel,
+                       uchar4* outputImageRGBA,
+                       int numRows,
+                       int numCols)
+{
+  const int2 thread_2D_pos = make_int2( blockIdx.x * blockDim.x + threadIdx.x,
+                                        blockIdx.y * blockDim.y + threadIdx.y);
 
-	// Allocate memory
-	int size = rows * cols;
-	checkCuda(cudaMalloc((void**)&input_image, size * sizeof(uchar4)));
-	checkCuda(cudaMalloc((void**)&output_image, size * sizeof(uchar4)));
-	checkCuda(cudaMemset(output, 0 , size * sizeof(unsigned char)));
+  const int thread_1D_pos = thread_2D_pos.y * numCols + thread_2D_pos.x;
+
+  //make sure we don't try and access memory outside the image
+  //by having any threads mapped there return early
+  if (thread_2D_pos.x >= numCols || thread_2D_pos.y >= numRows)
+    return;
+
+  unsigned char red   = redChannel[thread_1D_pos];
+  unsigned char green = greenChannel[thread_1D_pos];
+  unsigned char blue  = blueChannel[thread_1D_pos];
+
+  //Alpha should be 255 for no transparency
+  uchar4 outputPixel = make_uchar4(red, green, blue, 255);
+
+  outputImageRGBA[thread_1D_pos] = outputPixel;
+}
+
+__global__ void gaussianBlur(const unsigned char* const input,
+							  unsigned char* const output, 
+							  int rows, int cols,
+							  const float* const filter, const int filter_width)
+{
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	int y = blockIdx.y * blockDim.y + threadIdx.y;
+	if (x >= cols || y >= rows)
+		return;
 	
+	float c = 0.0f;
+	for (int fx = 0; x < filter_width; x++) {
+		for (int fy = 0; fy < filter_width; y++) {
+			int imagex = x + fx - filter_width / 2;
+			int imagey = y + fy - filter_width / 2;
+			imagex = min(max(imagex, 0), cols - 1);
+			imagey = min(max(imagey, 0), rows - 1);
+			c += (filter[fy * filter_width + fx] * input[imagey * cols + imagex]);
+		}
+	}	
+	output[y * cols + x] = c;
+}
+
+void imageBlur (const uchar4* const input,
+			    uchar4* output,
+				unsigned char* red_blurred,
+                unsigned char* green_blurred,
+                unsigned char* blue_blurred,
+			    unsigned int rows,
+			    unsigned int cols,
+				const float* const h_filter,
+				int filter_width) {
+
 	// block and grid size
 	int block_width = 16; // tile
 	const dim3 block_size(block_width, block_width);	
@@ -39,15 +103,34 @@ int imageBlur (unsigned char *input,
 	int blocks_y = (rows + block_width - 1) / block_width;
 	const dim3 grid_size(blocks_x, blocks_y);	
 
-	// copy to GPU
-	checkCuda(cudaMemcpy(input_image, input, size * sizeof(uchar4), cusaMemcpyHostToDevice));
+	// allocate memory and copy to GPU
+	checkCuda(cudaMalloc(&red, sizeof(unsigned char) * rows * cols));
+	checkCuda(cudaMalloc(&green, sizeof(unsigned char) * rows * cols));
+	checkCuda(cudaMalloc(&blue, sizeof(unsigned char) * rows * cols));
+	checkCuda(cudaMalloc(&d_filter, sizeof(float) * filter_width * filter_width));
+	checkCuda(cudaMemcpy(d_filter, h_filter, sizeof(float) * filter_width * filter_width, cudaMemcpyHostToDevice));
+
+	// separate channels
+	separateChannels<<<grid_size, block_size>>>(input, rows, cols, red, green, blue);
+
+	//call gaussian blur kernel on each channel
+	gaussianBlur <<<grid_size, block_size>>>(red, red_blurred, rows, cols, d_filter, filter_width);
+	gaussianBlur <<<grid_size, block_size>>>(green, green_blurred, rows, cols, d_filter, filter_width);
+	gaussianBlur <<<grid_size, block_size>>>(blue, blue_blurred, rows, cols, d_filter, filter_width);
 	cudaDeviceSynchronize();
 
-	// gaussian blur on channel
-	filter_width = ;
-	gaussianBlur <<<grid_size, block_size, filter_width * filter_width * sizeof(float)>>>(red, red_blurred, rows, cols, d_filer, filter_width);
-	cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+	//combine channels
+	combineChannels<<<grid_size, block_size>>>(red_blurred, 
+											   green_blurred,
+											   blue_blurred,
+											   output,
+											   rows,
+											   cols);
+	cudaDeviceSynchronize();
 
 	// free memory
-
+	cudaFree(red);
+	cudaFree(green);	
+	cudaFree(blue);
+	cudaFree(d_filter);
 }
--- cuda/imageBlur/main.cpp
+++ cuda/imageBlur/main.cpp
@@ -2,53 +2,110 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
-#include <cstdio>
+#include <stdint.h>
+#include <stdio.h>
 #include <cstdlib>
 #include <iostream>
 #include <ctime>
+#include <cuda.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace cv;
 using namespace std;
 
-// CUDA kernel
-//extern "C" __global__ void imageBlur();
+cv::Mat inputRGBA;
+cv::Mat outputRGBA;
 
-extern void imageBlur() {};
+inline cudaError_t checkCuda(cudaError_t result) {
+	#if defined(DEBUG) || defined(_DEBUG)
+		if (result != cudaSuccess) {
+			fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
+			exit(-1);
+		}
+	#endif
+		return result;
+}
+
+extern void imageBlur(const uchar4* const h_input,
+					  const uchar4* const d_input,
+					  uchar4* d_ouput,
+					  unsigned int rows,
+					  unsigned int cols, 
+					  unsigned char *red_blurred,
+					  unsigned char *green_blurred,
+					  unsigned char *blue_blurred,
+					  const float* const h_filter,
+					  const int filter_width);
 
 int main( int argc, const char** argv ) {
-    // arg 1: Input image 
-    // arg 2: output file name
         
     // Check inputs
-    if (argc != 3){
+    /*if (argc != 3){
         cout << "Incorrect number of inputs" << endl;
         cout << argv[0] << " <input file> <output file name>" << endl;
         return -1;
-    }        
+    } */       
         
     // Read input image from argument
-    Mat input_image = imread(argv[1], IMREAD_COLOR);
+    Mat input_image = imread("input.jpg", IMREAD_COLOR); //argv[1]
     if (input_image.empty()){
         cout << "Image cannot be loaded..!!" << endl;
         return -1;
     } 
-    unsigned int height = input_image.rows;
-    unsigned int  width = input_image.cols;
+    unsigned int rows = input_image.rows;
+    unsigned int cols = input_image.cols;
 
-	// Separate Color Channels      
-	Mat image_channels[3];
-	split(input_image, image_channels);
+	cv::cvtColor(input_image, inputRGBA, CV_BGR2RGBA);
+	outputRGBA.create(rows, cols, CV_8UC4);
+	*h_input = (uchar4 *)inputRGBA.ptr<unsigned char>(0);
+	*h_output = (uchar4 *)outputRGBA.ptr<unsigned char>(0);	
 
-	// blur each channel in GPU
-	
+	float *h_filter;
+	int filter_width;
 
-	// combine channels
-	Mat output_image = Mat::zeros(height, width, CV_8U);
+	//allocate memory and copy
+	uchar4 d_input, d_output;
+	unsigned char *red_blurred, *green_blurred, *blue_blurred;
+	cudaMalloc(d_input, sizeof(uchar4) * rows * cols);
+	cudaMalloc(d_output, sizeof(uchar4) * rows * cols);
+	cudaMemset(*d_output, 0, rows * cols * sizeof(uchar4));
+	cudaMemcpy(*d_input, *h_input, sizeof(uchar4) * rows * cols, cudaMemcpyHostToDevice);
+	cudaMalloc(red_blurred, sizeof(unsigned char) * rows * cols);
+	cudaMalloc(green_blurred, sizeof(unsigned char) * rows * cols);
+	cudaMalloc(blue_blurred, sizeof(unsigned char) * rows * cols);
+	cudaMemset(red_blurred, 0, sizeof(unsigned char) * rows * cols);
+	cudaMemset(green_blurred, 0, sizeof(unsigned char) * rows * cols);
+	cudaMemset(blue_blurred, 0, sizeof(unsigned char) * rows * cols);
 
+	// create filter
+	const int blurKernelWidth = 9;
+  	const float blurKernelSigma = 2.;
+	filter_width = blurKernelWidth;
+	float* h_filter = (float*)malloc(filter_width * filter_width * sizeof(float));
+	checkCuda(cudaMalloc(&h_filter, filter_width * filter_width * sizeof(float)));
+	*h_filter__ = *h_filter;
+	float filterSum = 0.f; //for normalization
+	int r, c;
+	for (r = -filter_width / 2; r <= filter_width / 2; ++r) {
+    	for (c = -filter_width/2; c <= filter_width / 2; ++c) {
+      		float filterValue = expf( -(float)(c * c + r * r) / (2.f * blurKernelSigma * blurKernelSigma));
+      		(*h_filter)[(r + blurKernelWidth/2) * blurKernelWidth + c + blurKernelWidth/2] = filterValue;
+      		filterSum += filterValue;
+    	}
+  	}
+  	float normalizationFactor = 1.f / filterSum;
+  	for (int r = -blurKernelWidth/2; r <= blurKernelWidth/2; ++r) {
+    	for (int c = -blurKernelWidth/2; c <= blurKernelWidth/2; ++c) {
+      		(*h_filter)[(r + blurKernelWidth/2) * blurKernelWidth + c + blurKernelWidth/2] = normalizationFactor;
+    	}
+  	}	
+
+	// call external function
+	imageBlur(h_input, d_input, d_output, rows, cols, red_blurred, green_blurred, blue_blurred, h_filter, filter_width);
 
 	// Write to image
-    cout << "writing output image " << argv[2] << endl;
-    imwrite (argv[2], output_image);
+    imwrite ("output.jpg", h_output);
 
     return 0;
 }
